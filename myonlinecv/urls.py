a225a15fa40928a5acdaf574f2516f34b248cf37
Mauro Chumpitaz Polino <mchumpitazp@gmail.com>
2022-06-23 13:27:58 +0300

    Media urls on debug false

--- myonlinecv/urls.py
+++ myonlinecv/urls.py
@@ -12,5 +12,4 @@ urlpatterns = [
     path("project_languages/<int:project_id>", views.project_languages, name="project_languages")
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
