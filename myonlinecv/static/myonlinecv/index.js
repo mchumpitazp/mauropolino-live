a3dd2cf77a2dbf0909d64bea7aae0990b35968cc
Mauro Chumpitaz Polino <mchumpitazp@gmail.com>
2022-07-01 14:02:36 +0300

    iOS detection function and Harvard record added

--- myonlinecv/static/myonlinecv/index.js
+++ myonlinecv/static/myonlinecv/index.js
@@ -379,6 +379,19 @@ function headerNavbar() {
 
 }
 
+function iOSDetect() {
+    return [
+      'iPad Simulator',
+      'iPhone Simulator',
+      'iPod Simulator',
+      'iPad',
+      'iPhone',
+      'iPod'
+    ].includes(navigator.platform)
+    // iPad on iOS 13 detection
+    || (navigator.userAgent.includes("Mac") && "ontouchend" in document)
+}
+
 function browserDetect() {
                  
     let userAgent = navigator.userAgent;
